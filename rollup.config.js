df636a825ce970022cfa7a4f318595fddba6a3da
Johan Martin Aarstein <johan@aarstein.media>
2025-03-20 09:43:19 +0100

    stash

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
+import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import autoprefixer from 'autoprefixer'
 import { readFile } from 'fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import flexbugs from 'postcss-flexbugs-fixes'
-import { dts } from 'rollup-plugin-dts'
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 import { summary } from 'rollup-plugin-summary'
@@ -25,73 +23,38 @@ const isProd = process.env.NODE_ENV !== 'development',
   ),
   input = path.resolve(__dirname, 'src', 'index.ts'),
   /**
-   * @type {import('rollup').RollupOptions.InputPluginOption}
-   * */
-  plugins = (preferBuiltins = false) => [
-    typescriptPaths(),
-    postcss({
-      inject: false,
-      plugins: isProd
-        ? [
-            flexbugs(),
-            autoprefixer({
-              flexbox: 'no-2009',
-            }),
-          ]
-        : [],
-    }),
-    template({
-      include: [
-        path.resolve(__dirname, 'src', 'elements', 'DotLottiePlayer.ts'),
-        path.resolve(__dirname, 'src', 'templates', '*'),
-      ],
-      options: {
-        shouldMinify({ parts }) {
-          return parts.some(
-            ({ text }) =>
-              text.includes('<figure') ||
-              text.includes('<div') ||
-              text.includes('<svg')
-          )
-        },
-      },
-    }),
-    json({
-      compact: true,
-    }),
-    nodeResolve({
-      extensions: ['.ts'],
-      preferBuiltins,
-    }),
-    commonjs(),
-    swc(),
-    isProd && minify(),
-    isProd && summary(),
-    !isProd &&
-    serve({
-      open: true,
-    }),
-    !isProd && livereload(),
-  ],
-  /**
    * @type {import('rollup').RollupOptions}
    * */
   unpkg = {
     input,
-    onwarn(warning, warn) {
-      if (warning.code === 'CIRCULAR_DEPENDENCY') {
-        return
-      }
-      warn(warning)
-    },
+    // onwarn(warning, warn) {
+    //   if (warning.code === 'CIRCULAR_DEPENDENCY') {
+    //     return
+    //   }
+    //   warn(warning)
+    // },
     output: {
       exports: 'named',
       extend: true,
-      file: pkg.unpkg,
+      file: pkg.main,
       format: 'iife',
       name: pkg.name,
     },
-    plugins: plugins(),
+    plugins: [
+      typescriptPaths(),
+      nodeResolve({
+        extensions: ['.ts'],
+      }),
+      commonjs(),
+      swc(),
+      isProd && minify(),
+      isProd && summary(),
+      !isProd &&
+      serve({
+        open: true,
+      }),
+      !isProd && livereload(),
+    ],
   }
 
 export default unpkg
