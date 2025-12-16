04465a3646bc8dd1635396100ed02fd747996aa3
Johan Martin Aarstein <johan@aarstein.media>
2025-12-16 10:09:39 +0100

    first commit

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export interface InitData {
   departments?: string
   departmentSelect: null | string
   feed: string
-  jobsWidget: HTMLDivElement
+  jobsWidget: HTMLDivElement | null
   languageSelect: string | null
   limit: number | null
   locations?: string
@@ -26,21 +26,13 @@ export interface InitData {
   url?: string
 }
 
-export interface Locations {
-  [id: string]: Location
-}
+export interface Locations {[id: string]: Location}
 
-export interface Departments {
-  [id: string]: Department
-}
+export interface Departments {[id: string]: Department}
 
-export interface Roles {
-  [id: string]: Role
-}
+export interface Roles {[id: string]: Role}
 
-export interface Regions {
-  [id: string]: Region
-}
+export interface Regions {[id: string]: Region}
 
 interface Unit {
   attributes: {
@@ -48,26 +40,16 @@ interface Unit {
     city: string
   }
   id: string
-  links: {
-    self: string
-  }
+  links: { self: string }
 }
 
-export interface Region extends Unit {
-  type: 'regions'
-}
+export interface Region extends Unit {type: 'regions'}
 
-export interface Department extends Unit {
-  type: 'departments'
-}
+export interface Department extends Unit {type: 'departments'}
 
-export interface Location extends Unit {
-  type: 'locations'
-}
+export interface Location extends Unit {type: 'locations'}
 
-export interface Role extends Unit {
-  type: 'roles'
-}
+export interface Role extends Unit {type: 'roles'}
 
 export interface ReturnType {
   data?: unknown[]
@@ -124,7 +106,7 @@ export interface JobData {
   }
   location_name?: string
   name?: string
-  relationships: {
+  relationships?: {
     activities: {
       links: {
         self: string
