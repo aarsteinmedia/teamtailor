b2274cd4c20f3968560e1fa717ae250ab101aae4
Johan Martin Aarstein <johan@aarstein.media>
2025-12-16 13:22:13 +0100

    stash

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,31 +1,24 @@
+import type { RollupOptions } from 'rollup'
+
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { readFile } from 'fs/promises'
+import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import livereload from 'rollup-plugin-livereload'
-import serve from 'rollup-plugin-serve'
-import { summary } from 'rollup-plugin-summary'
+import serve from 'rollup-plugin-opener'
+import pluginSummary from 'rollup-plugin-summary'
 import { minify, swc } from 'rollup-plugin-swc3'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 
 const isProd = process.env.NODE_ENV !== 'development',
   __dirname = path.dirname(fileURLToPath(import.meta.url)),
-  /**
-   * @type {typeof import('./package.json')}
-   * */
-  pkg = JSON.parse(
-    (
-      await readFile(
-        new URL(path.resolve(__dirname, 'package.json'), import.meta.url)
-      )
-    ).toString()
+  pkgBuffer = await readFile(new URL(path.resolve(__dirname, 'package.json'), import.meta.url)),
+  pkg: typeof import('./package.json') = JSON.parse(pkgBuffer.toString()),
+  input = path.resolve(
+    __dirname, 'src', 'index.ts'
   ),
-  input = path.resolve(__dirname, 'src', 'index.ts'),
-  /**
-   * @type {import('rollup').RollupOptions}
-   * */
-  unpkg = {
+  unpkg: RollupOptions = {
     input,
     // onwarn(warning, warn) {
     //   if (warning.code === 'CIRCULAR_DEPENDENCY') {
@@ -42,17 +35,13 @@ const isProd = process.env.NODE_ENV !== 'development',
     },
     plugins: [
       typescriptPaths(),
-      nodeResolve({
-        extensions: ['.ts'],
-      }),
+      nodeResolve({ extensions: ['.ts'] }),
       commonjs(),
       swc(),
       isProd && minify(),
-      isProd && summary(),
+      isProd && pluginSummary(),
       !isProd &&
-        serve({
-          open: true,
-        }),
+      serve({ open: true }),
       !isProd && livereload(),
     ],
   }
